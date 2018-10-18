fc0dc9eb2ef2cc139f101f711e23fc8848c721c0
sveta0112 <sveta0112@hotmail.com>
2018-10-17 22:30:38 -0700

    ble to convert json to csv, able to choose file, not fully completed.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,30 @@
+var convertCSV = (obj) => {
+    var keys = [];
 
+    if (keys.length === 0) {
+        for (let key in obj) {
+            if (key !== 'children') {
+                keys.push(key)
+            }
+        }
+    }
+    var results = [];
 
+    for (let key of keys) {
+        results.push(obj[key]);
+    }
+    let csvFile = keys.join(',') + '\r\n';
+    if (csvFile === undefined) {
+        csvFile = '';
+    }
+    csvFile += results.join(',') + '\r\n';
+
+    if (obj.children.length > 0) {
+       for (let i = 0; i <obj.children.length; i++) {
+           csvFile += convertCSV(obj.children[i], keys);
+       }
+    }
+    return csvFile;
+};
+
+module.exports = convertCSV;
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+var convertCSV = require('./client/app.js');
+var fs = require('fs');
+const app = express();
+const path = require('path');
+app.use(multer().single('text'));
 
+app.use(express.static('client'));
 
+app.post('/form', function(req, res){
+  var dats = JSON.parse(req.file.buffer.toString()); 
+  dats = convertCSV(dats);
+  console.log (dats);
+  res.send(dats );
+});
+
+app.listen(3000,function(){
+  console.log('Express server started at port 3000');
+});
